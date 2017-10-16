7fb875fdd6d1d9507ffbe2bb9ce6f928cea642bc
student <student@S006-8NQ185J-U>
2017-10-16 13:30:36 +0200

    oki

--- Labirynt/cwiczenie1/__init__.py
+++ Labirynt/cwiczenie1/__init__.py
@@ -9,15 +9,19 @@ class Labirynt():
         for i in range(0, len(read_data)):
             j = 0
             while j < len(read_data[i]):
-                print read_data[i][j]
-                self.lab += str(read_data[j][i])
+                #print read_data[i][j]
+                self.lab += str(read_data[i][j])
+                if read_data[i][j] == 'n':
+                    self.lab += '\n'
+                    j = 100
                 j += 1
-            
-             
-
             
     def printt(self):
         return self.lab
+    
+    def find(self):
+        szukaj = $
+        
     
 L1 = Labirynt()
 print L1.printt()
