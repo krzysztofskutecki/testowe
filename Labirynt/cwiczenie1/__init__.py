f9d111a1ea2b6cd0e15e4b2f057353ab8a302673
krzysztofskutecki <32837409+krzysztofskutecki@users.noreply.github.com>
2017-10-22 21:52:31 +0200

    Update __init__.py

--- Labirynt/cwiczenie1/__init__.py
+++ Labirynt/cwiczenie1/__init__.py
@@ -5,22 +5,66 @@ print read_data
 
 class Labirynt():
     def __init__(self):
-        self.lab = ''
+        self.lab_str = ''
+        self.lab_tab = []
+        self.wyjscie = [0,0]
+        self.pozycja = [0,0]
         for i in range(0, len(read_data)):
             j = 0
+            a = []
             while j < len(read_data[i]):
                 #print read_data[i][j]
-                self.lab += str(read_data[i][j])
+                self.lab_str += str(read_data[i][j])
+                a.append(read_data[i][j])
+                if read_data[i][j] == '$':
+                    self.wyjscie = read_data[i][j]
+                if read_data[i][j] == '@': 
+                    self.pozycja = read_data[i][j]
                 if read_data[i][j] == 'n':
-                    self.lab += '\n'
+                    self.lab_str += '\n'
                     j = 100
                 j += 1
+                self.lab_tab.append(a)
+        self.wysokosc = len(self.lab_tab)-1
+        self.szerokosc = len(self.lab_tab[1])-1
             
     def printt(self):
-        return self.lab
-    
+        return self.lab, self.szerokosc, self.wysokosc
+        
+    def lewo(self):
+        self.pozycja[0] - 1 = self.pozycja[0]
+        self.lab_tab[self.pozycja] = '*'
+    def prawo(self):
+        self.pozycja[0] + 1 = self.pozycja[0]
+        self.lab_tab[self.pozycja] = '*'
+    def gora(self):
+        self.pozycja[1] + 1 = self.pozycja[0]
+        self.lab_tab[self.pozycja] = '*'
+    def dol(self):
+        self.pozycja[1] - 1 = self.pozycja[0]
+        self.lab_tab[self.pozycja] = '*'
+        
     def find(self):
-        szukaj = $
+        while self.wyjscie != self.pozycja:
+            if self.pozycja[0] + 1 != '#' or self.pozycja[0] + 1 != '*' or self.pozycja[0] + 1 != '@':
+                self.prawo()
+            if self.pozycja[1] - 1 != '#' or self.pozycja[1] - 1 != '*' or self.pozycja[1] - 1 != '@':
+                self.dol()
+            if self.pozycja[0] - 1 != '#' or self.pozycja[0] - 1 != '*' or self.pozycja[0] - 1 != '@':
+                self.lewo()
+            if self.pozycja[1] + 1 != '#' or self.pozycja[1] + 1 != '*' or self.pozycja[1] + 1 != '@':
+                self.gora()
+            if self.pozycja[0] + 1 != '#':
+                self.prawo()
+            if self.pozycja[1] - 1 != '#':
+                self.dol()
+            if self.pozycja[0] - 1 != '#':
+                self.lewo()
+            if self.pozycja[1] + 1 != '#':
+                self.gora()
+        return self.lab_tab
+        
+        
         
     
 L1 = Labirynt()
